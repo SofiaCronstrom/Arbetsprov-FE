4c828795d3cd882da546fb6eb87b9018cc685da5
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-09-13 17:32:38 +0200

    adding media queries to App.css

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,20 +2,21 @@ import React from 'react';
 
 const Weather = () => {
 
-  const key = 'a94c8b198195fc9bc750b4799116fa83';
-
-  const getWeather = async => {
-
-    const url = 'http://api.weatherstack.com/current'
-  }
-
 
     return ( 
     <div className="wrapper">
-     <form className="location-input">
-            <label for="city">Enter a location for weather information</label>
-            <input type="text" name="city" className="form"/>
-          </form>
+     <body>
+              <form className="location-input">
+                <label className="label" for="city">Hur är vädret i...</label>
+                <input type="text" name="city" className="input"/>
+              </form>
+
+              <div className="container">
+                  <div className="box"></div>
+                  <div className="box"></div>
+                  <div className="box"></div>
+              </div>
+     </body>
     </div> 
     );
 }
